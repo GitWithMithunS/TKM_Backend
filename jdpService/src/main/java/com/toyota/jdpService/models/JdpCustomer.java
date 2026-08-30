bdd300205afeb0b9063f111266e176407f7e6281
GitWithMithunS <chakravarthimithun62@gmail.com>
2026-08-30 23:16:15 +0530

    added isDisabled option to the jdp customer master entity

--- jdpService/src/main/java/com/toyota/jdpService/models/JdpCustomer.java
+++ jdpService/src/main/java/com/toyota/jdpService/models/JdpCustomer.java
@@ -1,6 +1,7 @@
 package com.toyota.jdpService.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -34,4 +35,8 @@ public class JdpCustomer {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
+    @Column(name = "is_disabled")
+    @NotNull(message = "isDisabled cannot be null")
+    private Boolean isDisabled = false;
 }
